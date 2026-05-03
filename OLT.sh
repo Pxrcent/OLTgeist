005b7fec299e1d6237f4e5610ef624258d840331
LowPxrcentage <loolzynwoo@gmail.com>
2026-05-03 01:16:02 -0300

    so far so good

--- OLT.sh
+++ OLT.sh
@@ -1,46 +1,77 @@
 #!/usr/bin/env bash
 
-SN=$(echo "$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM$RANDOM" | head -c 8)
-
 #================#
+GEISTDIR="$HOME/.config/OLTgeist/"
+#================#
+gen_onu_json() {
+    local sn="$1"
+    local file="ONU-$sn.json"
+	
+    cat > "$file" <<EOF
+{
+  "serial_number": "$sn",
+  "Slot/PON/ID": "x/y/z",
+  "status": "online",
+  "rx_power_dbm": "-$(shuf -i 13-33 -n1).$(shuf -i 0-99 -n1)dB",
+  "tx_power_dbm": "$((RANDOM % 5 + 1))",
+  "temperature_c": "$(shuf -i 30-90 -n1)ºC",
+  "LAN_address": "192.168.1.1",
+  "WAN_address": "to do...",
+  "last_updated_info": "$(date +%F_%T)"
+}
+EOF
+}
+#================#
+
 while true; do
-  	read -p "OLTgeist<${CTX:-#}> " cmdy
+  	read -e -p "OLTgeist<${CTX:-#}> " cmdy
   
 case "$cmdy" in
 
 	"gen onu")
-	touch ONU-"$SN"
-	echo "onu created."
+if [[ "$CTX" != "" ]]; then # checking if its inside a Slot-PON folder before creating a ONU
+
+		SN=$(echo "$RANDOM$RANDOM" | md5sum | tr [:lower:] [:upper:] | head -c 8) # generates a random SN for each ONU
+		gen_onu_json "$SN"
+		echo "onu "$SN" created on Slot-PON "$CTX"."
+		echo "$CTX"
+	else
+		echo "no Slot-PON selected."
+		echo "select one using 'use X-Y'"
+		echo "[FAIL]"
+		continue
+fi
 ;;
-    "display ont 1")
-      jq '.' onts/ONT1.json
+    "display onu info") # wip
+      jq '.' ONU-*
 ;;
-    "ont 1 wifi set "*)
-      ssid=$(echo $cmd | cut -d' ' -f5)
-      jq ".wifi_ssid = \"$ssid\"" onts/ONT1.json > tmp && mv tmp onts/ONT1.json
-      echo "WiFi updated"
+    "ont wifi set"*) # wip
+  #    jq ".wifi_ssid = \"$ssid\"" onts/ONT1.json > tmp && mv tmp onts/ONT1.json
 ;;
-    "ont 1 remote enable")
-      jq '.remote_access = true' onts/ONT1.json > tmp && mv tmp onts/ONT1.json
-      echo "Remote access enabled"
+    "ont 1 remote enable") # wip
+  #    jq '.remote_access = true' onts/ONT1.json > tmp && mv tmp onts/ONT1.json
 ;;
 	use\ *)
-       dir=${cmdy#use }
-
-            if [[ $dir =~ ^[0-9]+-[0-9]+$ ]]; then
-                if [ -d "$dir" ]; then
-                    cd "$dir"
-                    CTX=${dir/-//}
-                else
-                    echo "Slot-PON not found"
-                fi
-            else
-                echo "Invalid format (use X-Y)"
-            fi	
+	 cmdyp=$(echo "$cmdy" | cut -c5-)		# cuts the word "use"
+	SLPN="$GEISTDIR$cmdyp"				# glues the dir to the path
+	if [[ -d "$SLPN" ]]; then			# check if it is created
+	
+		cd "$SLPN" 
+		echo "entering $PWD"
+		CTX="$cmdyp"			# changes the prompt
+		echo "[SUCC]"
+	else
+		echo "Slot-PON $SLPN does not exist"
+		echo "[FAIL]"
+		echo "returning to default..."
+		cd "$GEISTDIR"
+		CTX=""			# resets the prompt
+		continue
+	fi		 
 ;;
-	"show onu")
-		ls
-;;
+	"exit")
+		echo "terminating..."
+		exit 0 ;;
     *)
       continue
 ;;
